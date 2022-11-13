9c8b8bc579240ad97b52b4e9b4520b0e25ab4451
humaidan <humaidan@gmail.com>
2022-11-13 15:58:03 +0300

    csv save

--- main.py
+++ main.py
@@ -37,12 +37,46 @@ months = math.ceil(total_records / CONFIG['average_per_month'])
 
 recs = 0
 
+# last_date = datetime.datetime.strptime(CONFIG['last_date'], '%y-%m-%d %H:%M:%S')
+last_date = datetime.datetime.fromisoformat(CONFIG['last_date'])
+records_time_diff = CONFIG['average_per_month'] / 30 / 24 * 60  # mins
+last_count = 0
+
+
 while recs < total_records:
 
-    arrFlight = 
+    airlines = list(CONFIG['airlines_stats'].keys())
+    airlines_probs = list(CONFIG['airlines_stats'].values())
+    randFlights = random.choices(airlines, weights=airlines_probs, k=2)
+
+    arrFlight = randFlights[0] + random.choice(list(CONFIG['airlines_flightnums'][randFlights[0]]))
+    depFlight = randFlights[1] + random.choice(list(CONFIG['airlines_flightnums'][randFlights[1]]))
+
+    ACType = random.choice(CONFIG['ACType'])
+
+    pax = list(CONFIG['PAXBookLoad_stats'].keys())
+    pax_probs = list(CONFIG['PAXBookLoad_stats'].values())
+    bookload = random.choices(pax, weights=pax_probs, k=1)[0]
+
+    dests = list(CONFIG['dest_stats'].keys())
+    dests_probs = list(CONFIG['dest_stats'].values())
+    routes = random.choices(dests, weights=dests_probs, k=2)
+
+    time1 = random.choice( range(int(records_time_diff)) )
+    last_date -= datetime.timedelta(minutes=time1) + datetime.timedelta(seconds=random.choice( range(59) ))
+    sta_time = last_date
+    # print(last_date, time1)
+
+    delay_mins = random.choice( range(-99, 99) )
+    ata_time = sta_time + datetime.timedelta(minutes=delay_mins) + datetime.timedelta(seconds=random.choice( range(59) ))
+
+    # print(arrFlight, depFlight, ACType, bookload, route, sta_time, ata_time)
+    df.loc[len(df.index)] = [arrFlight, ACType, bookload, sta_time, ata_time, routes[0], routes[1]]
 
     recs += 1
-    pass
 
+print(df)
+df.to_csv('flights.csv')
+    
 
 
